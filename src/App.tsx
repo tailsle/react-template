99b2f0b59e8b2b1186a09a9064d7235bbbb8a8f0
terry <408718358@qq.com>
2023-02-23 23:14:28 +0800

    style: prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 /* eslint-disable @typescript-eslint/indent */
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import routes from './router'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import routes from './router';
 
 function App() {
 	return (
 		<Routes>
 			{routes.map(({ path, component: Component }) => {
-				return <Route path={path} key={path} element={<Component />}></Route>
+				return <Route path={path} key={path} element={<Component />}></Route>;
 			})}
 		</Routes>
-	)
+	);
 }
 
-export default App
+export default App;
